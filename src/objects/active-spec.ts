19fce9a99c553d3bbe44ffe6f7463ca4b6e6afcc
Gary Soedarsono <garysoed@gmail.com>
2021-05-12 18:27:44 -0700

    Leftover file

---
+++ src/objects/active-spec.ts
@@ -0,0 +1,29 @@
+import {$resolveState, $stateService, source} from 'grapevine';
+import {StateId} from 'gs-tools/export/state';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
+
+import {activeSpec, ActiveSpec} from '../core/active';
+
+
+export const $$activeSpec = source<BehaviorSubject<StateId<ActiveSpec>>>(
+    '$activeSpec',
+    vine => {
+      const stateService = $stateService.get(vine);
+      return new BehaviorSubject<StateId<ActiveSpec>>(
+          stateService.modify(x => x.add(
+              activeSpec({
+                $contentSpecs: x.add([]),
+              }),
+          )));
+    },
+);
+
+export const $activeSpec = source<Observable<ActiveSpec|undefined>>(
+    'activeSpec',
+    vine => $$activeSpec.get(vine).pipe(
+        switchMap(activeId => {
+          return activeId ? $resolveState.get(vine)(activeId) : of(undefined);
+        }),
+    ),
+);
