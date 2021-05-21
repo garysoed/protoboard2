c527c4fc785184dd281df24d932b8a3bbdca4947
Gary Soedarsono <garysoed@gmail.com>
2021-05-20 22:59:16 -0700

    Added trigger spec parser.

---
+++ src/core/trigger-spec.test.ts
@@ -0,0 +1,139 @@
+import {assert, objectThat, should, test} from 'gs-testing';
+
+import {DetailedTriggerSpec, triggerSpecParser, TriggerType, UnreservedTriggerKey} from './trigger-spec';
+
+
+test('@protoboard2/core/trigger-spec', () => {
+  test('BooleanFlagParser', () => {
+    test('convertBackward', () => {
+      should('convert true values correctly', () => {
+        assert(triggerSpecParser().convertBackward('p:meta')).to.haveProperties({
+          success: true,
+          result: objectThat<DetailedTriggerSpec<UnreservedTriggerKey>>().haveProperties({
+            type: TriggerType.P,
+            meta: true,
+          }),
+        });
+      });
+
+      should('convert false values correctly', () => {
+        assert(triggerSpecParser().convertBackward('p:nometa')).to.haveProperties({
+          success: true,
+          result: objectThat<DetailedTriggerSpec<UnreservedTriggerKey>>().haveProperties({
+            type: TriggerType.P,
+            meta: false,
+          }),
+        });
+      });
+
+      should('convert empty strings correctly', () => {
+        assert(triggerSpecParser().convertBackward('p:||')).to.haveProperties({
+          success: true,
+          result: TriggerType.P,
+        });
+      });
+
+      should('fail if the string doesn\'t match', () => {
+        assert(triggerSpecParser().convertBackward('p:blah')).to.haveProperties({
+          success: true,
+          result: TriggerType.P,
+        });
+      });
+    });
+
+    test('convertForward', () => {
+      should('convert undefined values correctly', () => {
+        assert(triggerSpecParser().convertForward({type: TriggerType.P, alt: undefined})).to.haveProperties({
+          success: true,
+          result: 'p',
+        });
+      });
+
+      should('convert true value correctly', () => {
+        assert(triggerSpecParser().convertForward({type: TriggerType.P, alt: true})).to.haveProperties({
+          success: true,
+          result: 'p:alt',
+        });
+      });
+
+      should('convert false value correctly', () => {
+        assert(triggerSpecParser().convertForward({type: TriggerType.P, meta: false})).to.haveProperties({
+          success: true,
+          result: 'p:nometa',
+        });
+      });
+    });
+  });
+
+  test('UnreservedTriggerSpecParser', () => {
+    test('convertBackward', () => {
+      should('convert trigger with options correctly', () => {
+        assert(triggerSpecParser().convertBackward('p:meta|alt|noctrl')).to.haveProperties({
+          success: true,
+          result: objectThat<DetailedTriggerSpec<UnreservedTriggerKey>>().haveProperties({
+            type: TriggerType.P,
+            alt: true,
+            ctrl: false,
+            meta: true,
+          }),
+        });
+      });
+
+      should('exclude invalid options', () => {
+        assert(triggerSpecParser().convertBackward('p:meta|blah|noctrl')).to.haveProperties({
+          success: true,
+          result: objectThat<DetailedTriggerSpec<UnreservedTriggerKey>>().haveProperties({
+            type: TriggerType.P,
+            ctrl: false,
+            meta: true,
+          }),
+        });
+      });
+
+      should('convert triggers without any options', () => {
+        assert(triggerSpecParser().convertBackward('p')).to.haveProperties({
+          success: true,
+          result: TriggerType.P,
+        });
+      });
+
+      should('fail if the type part is invalid', () => {
+        assert(triggerSpecParser().convertBackward('?')).to.haveProperties({
+          success: false,
+        });
+      });
+    });
+
+    test('convertForward', () => {
+      should('convert triggers with options correctly', () => {
+        const trigger = {
+          type: TriggerType.P,
+          alt: true,
+          ctrl: false,
+          meta: true,
+        } as const;
+        assert(triggerSpecParser().convertForward(trigger)).to.haveProperties({
+          success: true,
+          result: 'p:alt|noctrl|meta',
+        });
+      });
+
+      should('convert triggers without options correctly', () => {
+        const trigger = {
+          type: TriggerType.P,
+        } as const;
+        assert(triggerSpecParser().convertForward(trigger)).to.haveProperties({
+          success: true,
+          result: 'p',
+        });
+      });
+
+      should('convert simple triggers correctly', () => {
+        assert(triggerSpecParser().convertForward(TriggerType.P)).to.haveProperties({
+          success: true,
+          result: 'p',
+        });
+      });
+    });
+  });
+});
--- src/core/trigger-spec.ts
+++ src/core/trigger-spec.ts
@@ -1,3 +1,6 @@
+import {$asArray, $filterNonNull, $last, $map, $pipe, $scan} from 'gs-tools/export/collect';
+import {enumType, equalType, intersectType, notType, Type, unionType} from 'gs-types';
+import {Converter, Result} from 'nabu';
 import {Selector} from 'persona';
 
 export enum TriggerType {
@@ -32,7 +35,16 @@ export enum TriggerType {
 }
 
 type ReservedTriggerKey = TriggerType.QUESTION;
-type UnreservedTriggerKey = Exclude<TriggerType, ReservedTriggerKey>;
+export type UnreservedTriggerKey = Exclude<TriggerType, ReservedTriggerKey>;
+
+const RESERVED_TRIGGER_KEY_TYPE: Type<ReservedTriggerKey> = unionType([
+  equalType(TriggerType.QUESTION),
+]);
+
+const UNRESERVED_TRIGGER_KEY_TYPE: Type<UnreservedTriggerKey> = intersectType([
+  notType<TriggerType, ReservedTriggerKey>(RESERVED_TRIGGER_KEY_TYPE),
+  enumType<TriggerType>(TriggerType),
+]);
 
 export interface DetailedTriggerSpec<T> {
   readonly type: T;
@@ -49,4 +61,118 @@ export type TriggerSpec = TriggerType|DetailedTriggerSpec<TriggerType>;
 
 export function isKeyTrigger(triggerType: TriggerType): boolean {
   return triggerType !== TriggerType.CLICK;
+}
+
+class BooleanFlagParser implements Converter<boolean|undefined, string> {
+  private readonly trueStr = this.key;
+  private readonly falseStr = `no${this.key}`;
+
+  constructor(private readonly key: string) { }
+
+  convertBackward(value: string): Result<boolean|undefined> {
+    if (!value) {
+      return {success: true, result: undefined};
+    }
+
+    switch (value) {
+      case this.trueStr:
+        return {success: true, result: true};
+      case this.falseStr:
+        return {success: true, result: false};
+      default:
+        return {success: false};
+    }
+  }
+
+  convertForward(input: boolean|undefined): Result<string> {
+    if (input === undefined) {
+      return {success: true, result: ''};
+    }
+
+    const result = input ? this.trueStr : this.falseStr;
+    return {success: true, result};
+  }
+}
+
+class UnreservedTriggerSpecParser implements Converter<UnreservedTriggerSpec, string> {
+  private readonly altParser = new BooleanFlagParser('alt');
+  private readonly ctrlParser = new BooleanFlagParser('ctrl');
+  private readonly metaParser = new BooleanFlagParser('meta');
+  private readonly shiftParser = new BooleanFlagParser('shift');
+
+  convertBackward(value: string): Result<UnreservedTriggerSpec> {
+    const [typePart, options] = value.split(':');
+    if (!UNRESERVED_TRIGGER_KEY_TYPE.check(typePart)) {
+      return {success: false};
+    }
+
+    if (!options) {
+      return {success: true, result: typePart};
+    }
+
+    const triggerOptions = $pipe(
+        options.split('|'),
+        $map(optionPart => {
+          const altResult = this.altParser.convertBackward(optionPart);
+          if (altResult.success && altResult.result !== undefined) {
+            return {alt: altResult.result};
+          }
+
+          const ctrlResult = this.ctrlParser.convertBackward(optionPart);
+          if (ctrlResult.success && ctrlResult.result !== undefined) {
+            return {ctrl: ctrlResult.result};
+          }
+
+          const metaResult = this.metaParser.convertBackward(optionPart);
+          if (metaResult.success && metaResult.result !== undefined) {
+            return {meta: metaResult.result};
+          }
+
+          const shiftResult = this.shiftParser.convertBackward(optionPart);
+          if (shiftResult.success && shiftResult.result !== undefined) {
+            return {shift: shiftResult.result};
+          }
+
+          return null;
+        }),
+        $filterNonNull(),
+        $scan((item, acc) => ({...acc, ...item}), {}),
+        $asArray(),
+        $last(),
+    );
+
+    if (!triggerOptions) {
+      return {success: true, result: typePart};
+    }
+
+    return {success: true, result: {...triggerOptions, type: typePart}};
+  }
+
+  convertForward(input: UnreservedTriggerSpec): Result<string> {
+    if (typeof input === 'string') {
+      return {success: true, result: input};
+    }
+
+    const options = $pipe(
+        [
+          this.altParser.convertForward(input.alt),
+          this.ctrlParser.convertForward(input.ctrl),
+          this.metaParser.convertForward(input.meta),
+          this.shiftParser.convertForward(input.shift),
+        ],
+        $map(result => (result.success ? result.result : null) || null),
+        $filterNonNull(),
+        $asArray(),
+    ).join('|');
+
+    if (!options) {
+      return {success: true, result: input.type};
+    }
+
+    return {success: true, result: `${input.type}:${options}`};
+  }
+}
+
+export function triggerSpecParser(): Converter<UnreservedTriggerSpec, string> {
+  return new UnreservedTriggerSpecParser();
 }
