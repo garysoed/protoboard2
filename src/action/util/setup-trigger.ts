2e646b5973ef144c41894187c0bca0b37c2c81b6
Gary Soedarsono <garysoed@gmail.com>
2021-06-01 23:22:27 -0700

    Refactored base component a bit.

---
+++ src/action/util/setup-trigger.ts
@@ -0,0 +1,83 @@
+import {host, onDom, PersonaContext} from 'persona';
+import {EMPTY, fromEvent, merge, Observable} from 'rxjs';
+import {filter, map, mapTo, switchMap, throttleTime, withLatestFrom} from 'rxjs/operators';
+
+import {TriggerEvent} from '../../core/trigger-event';
+import {DetailedTriggerSpec, isKeyTrigger, TriggerSpec, TriggerType} from '../../core/trigger-spec';
+import {NormalizedTriggerConfig, TriggerConfig} from '../action-spec';
+
+
+type RawTriggerEvent = (KeyboardEvent|MouseEvent)&TriggerEvent;
+
+function createTriggerClick(
+    triggerSpec: DetailedTriggerSpec<TriggerSpec>,
+    context: PersonaContext,
+): Observable<MouseEvent&TriggerEvent> {
+  const targetEl = triggerSpec.targetEl ?? host({});
+  return onDom<MouseEvent>('click')
+      .resolve(context => targetEl.getSelectable(context))
+      .getValue(context)
+      .pipe(
+          map(event => {
+            return Object.assign(event, {mouseX: event.offsetX, mouseY: event.offsetY});
+          }),
+      );
+}
+
+function createTriggerKey(
+    triggerSpec: DetailedTriggerSpec<TriggerType>,
+    context: PersonaContext,
+): Observable<KeyboardEvent&TriggerEvent> {
+  const targetEl = triggerSpec.targetEl ?? host({});
+  const onMouseLeave$ = onDom('mouseleave')
+      .resolve(context => targetEl.getSelectable(context))
+      .getValue(context);
+  const onMouseEnter$ = onDom('mouseenter')
+      .resolve(context => targetEl.getSelectable(context))
+      .getValue(context);
+  const onMouseMove$ = onDom<MouseEvent>('mousemove')
+      .resolve(context => targetEl.getSelectable(context))
+      .getValue(context);
+  return merge(
+      onMouseLeave$.pipe(mapTo(false)),
+      onMouseEnter$.pipe(mapTo(true)),
+  )
+      .pipe(
+          switchMap(hovered => {
+            return hovered ? fromEvent<KeyboardEvent>(window, 'keydown') : EMPTY;
+          }),
+          withLatestFrom(onMouseMove$.pipe(throttleTime(10))),
+          filter(([event]) => event.key === triggerSpec.type),
+          map(([keyboardEvent, mouseEvent]) => {
+            return Object.assign(
+                keyboardEvent,
+                {mouseX: mouseEvent.offsetX, mouseY: mouseEvent.offsetY});
+          }),
+      );
+}
+
+export function createTrigger<C extends TriggerConfig>(
+    config$: Observable<NormalizedTriggerConfig<C>>,
+    context: PersonaContext,
+): Observable<TriggerEvent> {
+  return config$.pipe(
+      switchMap(config => {
+        if (!config.trigger) {
+          return EMPTY;
+        }
+
+        const triggerSpec = config.trigger;
+        const trigger$: Observable<RawTriggerEvent> = isKeyTrigger(triggerSpec.type)
+          ? createTriggerKey(triggerSpec, context)
+          : createTriggerClick(triggerSpec, context);
+        return trigger$.pipe(
+            filter(event => {
+              return event.altKey === (triggerSpec.alt ?? false)
+                && event.ctrlKey === (triggerSpec.ctrl ?? false)
+                && event.metaKey === (triggerSpec.meta ?? false)
+                && event.shiftKey === (triggerSpec.shift ?? false);
+            }),
+        );
+      }),
+  );
+}
--- src/core/base-component.ts
+++ src/core/base-component.ts
@@ -4,23 +4,21 @@ import {combineLatestObject} from 'gs-tools/export/rxjs';
 import {StateId} from 'gs-tools/export/state';
 import {mapObject} from 'gs-tools/export/typescript';
 import {BaseThemedCtrl, stateIdParser, _p} from 'mask';
-import {attributeIn, host, onDom, PersonaContext} from 'persona';
+import {attributeIn, host, PersonaContext} from 'persona';
 import {INPUT_TYPE} from 'persona/export/internal';
-import {combineLatest, EMPTY, fromEvent, merge, Observable, of} from 'rxjs';
-import {filter, map, mapTo, switchMap, throttleTime, withLatestFrom} from 'rxjs/operators';
+import {combineLatest, EMPTY, Observable, of} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
 import {Logger} from 'santa';
 
 import {ActionSpec, ConfigSpecs, NormalizedTriggerConfig, TriggerConfig} from '../action/action-spec';
 import {helpAction} from '../action/help-action';
 import {ActionTrigger} from '../action/help-service';
+import {createTrigger} from '../action/util/setup-trigger';
 
-import {TriggerEvent} from './trigger-event';
-import {DetailedTriggerSpec, isKeyTrigger, TriggerSpec, TriggerType, UnreservedTriggerSpec} from './trigger-spec';
+import {DetailedTriggerSpec, TriggerType, UnreservedTriggerSpec} from './trigger-spec';
 
 
 const LOG = new Logger('pb.core.BaseComponent');
-
-type RawTriggerEvent = (KeyboardEvent|MouseEvent)&TriggerEvent;
 
 
 type ObservableConfig<C> = {readonly [K in keyof C]: Observable<C[K]>};
@@ -71,51 +69,6 @@ export abstract class BaseComponent<O, S extends typeof $> extends BaseThemedCtr
         .pipe(switchMap(objectId => $resolveState.get(this.context.vine)(objectId)));
   }
 
-  private createTriggerClick(
-      triggerSpec: DetailedTriggerSpec<TriggerSpec>,
-  ): Observable<MouseEvent&TriggerEvent> {
-    const targetEl = triggerSpec.targetEl ?? host({});
-    return onDom<MouseEvent>('click')
-        .resolve(context => targetEl.getSelectable(context))
-        .getValue(this.context)
-        .pipe(
-            map(event => {
-              return Object.assign(event, {mouseX: event.offsetX, mouseY: event.offsetY});
-            }),
-        );
-  }
-
-  private createTriggerKey(
-      triggerSpec: DetailedTriggerSpec<TriggerType>,
-  ): Observable<KeyboardEvent&TriggerEvent> {
-    const targetEl = triggerSpec.targetEl ?? host({});
-    const onMouseLeave$ = onDom('mouseleave')
-        .resolve(context => targetEl.getSelectable(context))
-        .getValue(this.context);
-    const onMouseEnter$ = onDom('mouseenter')
-        .resolve(context => targetEl.getSelectable(context))
-        .getValue(this.context);
-    const onMouseMove$ = onDom<MouseEvent>('mousemove')
-        .resolve(context => targetEl.getSelectable(context))
-        .getValue(this.context);
-    return merge(
-        onMouseLeave$.pipe(mapTo(false)),
-        onMouseEnter$.pipe(mapTo(true)),
-    )
-        .pipe(
-            switchMap(hovered => {
-              return hovered ? fromEvent<KeyboardEvent>(window, 'keydown') : EMPTY;
-            }),
-            withLatestFrom(onMouseMove$.pipe(throttleTime(10))),
-            filter(([event]) => event.key === triggerSpec.type),
-            map(([keyboardEvent, mouseEvent]) => {
-              return Object.assign(
-                  keyboardEvent,
-                  {mouseX: mouseEvent.offsetX, mouseY: mouseEvent.offsetY});
-            }),
-        );
-  }
-
   private normalizeConfig<C extends TriggerConfig>(
       configSpecs: ConfigSpecs<C>,
   ): Observable<NormalizedTriggerConfig<C>> {
@@ -157,29 +110,11 @@ export abstract class BaseComponent<O, S extends typeof $> extends BaseThemedCtr
       actionSpec: ActionSpec<C>,
       config$: Observable<NormalizedTriggerConfig<C>>,
   ): Observable<unknown> {
-    return config$.pipe(
-        switchMap(config => {
-          if (!config.trigger) {
-            return EMPTY;
-          }
-
-          const triggerSpec = config.trigger;
-          const trigger$: Observable<RawTriggerEvent> = isKeyTrigger(triggerSpec.type)
-            ? this.createTriggerKey(triggerSpec)
-            : this.createTriggerClick(triggerSpec);
-          return trigger$.pipe(
-              filter(event => {
-                return event.altKey === (triggerSpec.alt ?? false)
-                    && event.ctrlKey === (triggerSpec.ctrl ?? false)
-                    && event.metaKey === (triggerSpec.meta ?? false)
-                    && event.shiftKey === (triggerSpec.shift ?? false);
-              }),
-              actionSpec.action({
-                config$,
-                objectId$: this.objectId$,
-                vine: this.context.vine,
-              }),
-          );
+    return createTrigger(config$, this.context).pipe(
+        actionSpec.action({
+          config$,
+          objectId$: this.objectId$,
+          vine: this.context.vine,
         }),
     );
   }
