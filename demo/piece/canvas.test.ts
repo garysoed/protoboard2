61aa1222c34f6f9d7ffdcbd46e7d782841cb5575
Gary Soedarsono <garysoed@gmail.com>
2021-06-06 17:31:42 -0700

    Added test for CanvasDemo.

---
+++ demo/piece/canvas.test.ts
@@ -0,0 +1,33 @@
+import {$stateService} from 'grapevine';
+import {assert, runEnvironment, should, test} from 'gs-testing';
+import {BrowserSnapshotsEnv} from 'gs-testing/export/browser';
+import {fakeStateService} from 'gs-tools/export/state';
+import {_p} from 'mask';
+import {THEME_LOADER_TEST_OVERRIDE} from 'mask/export/testing';
+import {PersonaTesterFactory} from 'persona/export/testing';
+
+import {CanvasDemo} from './canvas';
+import goldenDefault from './goldens/canvas.html';
+
+
+const TESTER_FACTORY = new PersonaTesterFactory(_p);
+test('@protoboard2/demo/piece/canvas', init => {
+  const _ = init(() => {
+    runEnvironment(new BrowserSnapshotsEnv({default: goldenDefault}));
+
+    const tester = TESTER_FACTORY.build({
+      rootCtrls: [CanvasDemo],
+      rootDoc: document,
+      overrides: [
+        THEME_LOADER_TEST_OVERRIDE,
+        {override: $stateService, withValue: fakeStateService()},
+      ],
+    });
+    const el = tester.createElement(CanvasDemo);
+    return {el};
+  });
+
+  should('render correctly', () => {
+    assert(_.el.flattenContent()).to.matchSnapshot('default');
+  });
+});
