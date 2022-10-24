3f6603ea1bbba4e86238d3cc6bdb09ec01def206
Gary Soedarsono <garysoed@gmail.com>
2022-10-24 10:39:28 -0700

    Exported ComponentId

--- export/index.ts
+++ export/index.ts
@@ -5,7 +5,7 @@ export {LENS} from '../src/face/lens';
 export {LENS_DISPLAY} from '../src/face/lens-display';
 export {SurfaceState, SURFACE, surfaceState, SURFACE_STATE_TYPE} from '../src/region/surface';
 
-export {componentId} from '../src/id/component-id';
+export {componentId, ComponentId} from '../src/id/component-id';
 
 export {renderForeignObject} from '../src/svg/render-foreign-object';
 
